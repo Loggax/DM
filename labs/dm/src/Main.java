6af24068495e7b74b4503dd42489bf8a74b0fc4e
Loggax <zaharlagoida2602@gmail.com>
2023-04-21 14:40:58 +0300

    labs dm

---
+++ labs/dm/src/Main.java
@@ -0,0 +1,18 @@
+import labs.Lab2;
+import labs.Lab3;
+import labs.Lab4;
+import labs.Lab5;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+       //Lab2 lab2 = new Lab2();
+        //lab2.start();
+   //Lab3 lab3 = new Lab3();
+        //lab3.start();
+    //Lab4 lab4 = new Lab4();
+      //lab4.start();
+      Lab5 lab5 = new Lab5();
+        lab5.start();
+    }
+}
